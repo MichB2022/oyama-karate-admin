fd1cc48807bb10bb5291d5c925098b313e87bb1c
Jakub Pisula <jakubpisula@macbook-pro-jakub-2.home>
2022-03-29 01:18:51 +0200

    a

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -5,18 +5,32 @@ export const httpRequest = async (method, url, data) => {
   axios.defaults.withCredentials = true;
   switch (method) {
     case 'GET':
-      return await axios.get(`${API_URL}${url}`);
+      return await axios(`${API_URL}${url}`, {
+        method: 'get',
+        withCredentials: true
+      });
+    // return await axios.get(`${API_URL}${url}`);
     case 'PUT':
-      return await axios.put(`${API_URL}${url}`, data);
+      return await axios(`${API_URL}${url}`, {
+        method: 'put',
+        data: data,
+        withCredentials: true
+      });
+    // return await axios.put(`${API_URL}${url}`, data);
     case 'POST':
       return await axios(`${API_URL}${url}`, {
         method: 'post',
-        data: someJsonData,
+        data: data,
         withCredentials: true
       });
     // return await axios.post(`${API_URL}${url}`, data, withCredentials: true);
     case 'DELETE':
-      return await axios.delete(`${API_URL}${url}`);
+      return await axios(`${API_URL}${url}`, {
+        method: 'delete',
+        // data: data,
+        withCredentials: true
+      });
+    // return await axios.delete(`${API_URL}${url}`);
     default:
       return;
   }
